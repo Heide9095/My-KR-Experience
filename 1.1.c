28a27ca9931ae4a28197a0a07ce5dd74874fd0b7
Heidemann <padma@heidemann.se>
2025-11-24 23:25:53 +0100

    Add files via upload

---
+++ 1.1.c
@@ -0,0 +1,10 @@
+// 1.1 Getting Started
+/*
+#include <stdio.h>			// standart in-/output header
+int main(){				// main integer, no argument
+	printf("hello, world\n");	// printf function(character string+nl)
+	return 0;			// return integer
+}					// complete programm
+*/
+
+// Exercise 1-1 and 1-2 are not necessary
