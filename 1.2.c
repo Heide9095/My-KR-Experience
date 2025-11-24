e19df8a15fcc234715569ef31e9870b53eb63826
Heidemann <padma@heidemann.se>
2025-11-24 23:26:41 +0100

    Add files via upload
    work in progress

---
+++ 1.2.c
@@ -0,0 +1,23 @@
+// 1.2 Variables and Arithmentic Expressions
+// C=(5/9)(F-32)
+
+/*
+#include <stdio.h>
+int main(){
+	int fahr, celcius;	// all variables are decalred before...
+	int lower, upper, step;	// ...used, at the beginning of a function
+
+	lower = 0;		// lower limit of temperature scale
+	upper = 300;		// upper limit
+	step = 20;		// step size
+
+	fahr = lower;
+	while (fahr <= upper) {	// while the argument is true execute function
+		celcius = 5 * (fahr-32) / 9;
+		printf("%d\t%d\n", fahr, celcius); // decimal tab decimal
+		fahr = fahr + step;		 
+	}
+}
+*/
+
+
